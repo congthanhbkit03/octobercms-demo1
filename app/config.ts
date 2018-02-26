1f4e11e35eab30f1136aa7cc63778c643bbb19bf
congthanhbkit03 <congthanhbkit03@gmail.com>
2018-02-26 20:31:12 +0700

    hjgjh

--- app/config.ts
+++
@@ -1,7 +0,0 @@
-// --- Data location ---
-export var DATA_FROM_SERVER = true;
-
-// --- Server URL ---
-export var SERVER_URL = 'http://pvhoang.vn/atm/atmData.php';
-// export var SERVER_URL = 'http://localhost/atm/atmData.php';
-
